271d9fcf5c13990deb61f661901b5e0c3568f4a7
fujimoto-lab <39648144+fujimoto-lab@users.noreply.github.com>
2019-01-08 00:29:53 +0900

    changed hook url

--- functions/deploy.js
+++ functions/deploy.js
@@ -1,7 +1,7 @@
 const https = require('https')
 
 // Netlify deploy URL
-const url = "https://api.netlify.com/build_hooks/5c0542ccb3127447fad1b0d9"
+const url = "https://api.netlify.com/build_hooks/5c0d4162e39e7c01ea49e81a"
 
 exports.handler = (event, context, callback) => {
   const hostname = url.match(/^https?:\/\/([^/]+)\/.*$/)[1]
